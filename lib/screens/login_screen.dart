ad32ae40445e66e38103829341d296b15764c709
FarhantheNoob <farhanactivitieshub@gmail.com>
2022-07-02 10:43:58 +0530

    final commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,25 +13,27 @@ class _LoginScreenState extends State<LoginScreen> {
   final _authMethods = AuthMethods();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Start or join a meeting',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: Image.asset('assets/onboarding.jpg'),
-          ),
-          CustomButton(text: 'Sign In to join meeting', onPressed: () async{
-            bool res = await _authMethods.signInWithGoogle(context);
-            if(res){
-              Navigator.pushNamed(context, '/home');
-            }
-          })
-        ],
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Start or join a meeting',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Image.asset('assets/onboarding.jpg'),
+            ),
+            CustomButton(text: 'Sign In to join meeting', onPressed: () async{
+              bool res = await _authMethods.signInWithGoogle(context);
+              if(res){
+                Navigator.pushNamed(context, '/home');
+              }
+            })
+          ],
+        ),
       ),
     );
   }
